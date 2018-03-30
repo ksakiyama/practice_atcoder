a1af0ea969254586589dfda118c91f50c5406abb
ksakiyama <k.sakiyama1@gmail.com>
2018-03-30 12:52:45 +0900

    fix bug

--- abc057c/main.cpp
+++ abc057c/main.cpp
@@ -4,22 +4,11 @@
 #include <algorithm>
 
 using namespace std;
-// vector<long> trial_division(long n) {
-//   vector<long> a;
-//   long f = 2;
-//   while (n > 1) {
-//     if (n % f == 0) {
-//       a.push_back(f);
-//       n /= f;
-//     } else {
-//       f += 1;
-//     }
-//   }
-//   return a;
-// }
+
+// 素因数分解する関数
+// 不要だった…
 vector<long> trial_division(long n) {
   vector<long> a;
-  a.push_back(1); // 後で
   for (long i = 2; i < long(sqrt(n) + 2); i++) {
     while (n % i == 0) {
       n /= i;
@@ -54,25 +43,14 @@ int main() {
   long n;
   cin >> n;
 
-  // for (auto v : trial_division(n)) {
-  //   cout << v << endl;
-  // }
+  long ans = num_digits(n);
 
-  vector<long> vec = trial_division(n);
+  for (long a = 1; a*a <= n; a++) {
+    if (n % a != 0) continue;
+    const long b = n / a;
+    ans = min(ans, max(num_digits(a), num_digits(b)));
+  }
 
-  const long m = vec.size();
-  long ans = 10000000000; // INF
-  do {
-    for (long i = 0; i < m-1; i++) {
-      long a = 1, b = 1;
-      for (long j = 0; j < i + 1; j++) {
-        a *= vec[j];
-      }
-      for (long j = i + 1; j < m; j++) {
-        b *= vec[j];
-      }
-      ans = min(ans, max(num_digits(a), num_digits(b)));
-    }
-  } while (next_permutation(vec.begin(), vec.end()));
   cout << ans << endl;
+  return 0;
 }
