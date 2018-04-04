badf0e0a72388a76fc153bf597d275e7c11fec83
ksakiyama <k.sakiyama1@gmail.com>
2018-04-04 10:12:40 +0900

    add template.cpp

---
+++ template.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <utility>
+
+#define REP(i,n) for(int i=0;i<n;i++)
+
+typedef long long ll;
+using namespace std;
+
+int main() {
+  return 0;
+}
