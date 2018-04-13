9f4a744db6804a26b9eed045137863a630b88ffa
ksakiyama <k.sakiyama1@gmail.com>
2018-04-13 09:03:56 +0900

    add a rust file written about standard input

---
+++ main.rs
@@ -0,0 +1,39 @@
+
+fn read() -> i32 {
+    let mut s = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    s.trim().parse().ok().unwrap()
+}
+
+
+fn read_line() -> Vec<i32> {
+    let mut s = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    let v = s.trim()
+        .split_whitespace()
+        .map(|e| e.parse().ok().unwrap())
+        .collect();
+    return v
+}
+
+
+fn read_lines(n : i32) -> Vec<Vec<i32>> {
+    let mut v2 = Vec::new();
+    for _ in 0..n {
+        let mut s = String::new();
+        std::io::stdin().read_line(&mut s).ok();
+        let v = s.trim()
+            .split_whitespace()
+            .map(|e| e.parse().ok().unwrap())
+            .collect();
+        v2.push(v);
+    }
+    v2
+}
+
+
+fn read_line_str() -> String {
+    let mut s = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    return s;
+}
