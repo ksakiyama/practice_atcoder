aff5d04387539dc44f7548fcbf11245ab6a1d441
ksakiyama <k.sakiyama1@gmail.com>
2018-04-17 11:37:32 +0900

    Add MyScanner

--- abc094c/main.go
+++ abc094c/main.go
@@ -9,12 +9,9 @@ import (
 )
 
 func main() {
-	n := readInt()
-	// vec := make([]int, 0)
-	// for i := 0; i < n; i++ {
-	// 	vec = append(vec, readInt())
-	// }
-	vec := readIntLine(n)
+	scanner := newMyScanner()
+	n := scanner.readInt()
+	vec := scanner.readIntLine(n)
 
 	// copy
 	cpVec := make([]int, len(vec))
@@ -33,32 +30,61 @@ func main() {
 			fmt.Println("ERR")
 		}
 	}
-
-	// fmt.Println(n)
-	// fmt.Println(vec)
-	// fmt.Println(cpVec)
 }
 
-func readInt() int {
-	var a int
-	fmt.Scan(&a)
-	return a
+// MyScanner : comment
+type MyScanner struct {
+	sc *bufio.Scanner
 }
 
-func readIntLine(n int) []int {
-	var sc = bufio.NewScanner(os.Stdin)
-	sc.Split(bufio.ScanWords)
+func newMyScanner() *MyScanner {
+	ms := new(MyScanner)
+	ms.sc = bufio.NewScanner(os.Stdin)
+	return ms
+}
+
+func (ms *MyScanner) readIntLine(n int) []int {
+	ms.sc.Split(bufio.ScanWords)
 
 	ret := make([]int, 0)
 
 	for i := 0; i < n; i++ {
-		sc.Scan()
-		v, e := strconv.Atoi(sc.Text())
-		if e != nil {
-			panic(e)
+		ms.sc.Scan()
+		v, err := strconv.Atoi(ms.sc.Text())
+		if err != nil {
+			panic(err)
 		}
 		ret = append(ret, v)
 	}
 
 	return ret
+}
+
+func (ms *MyScanner) readInt64Line(n int) []int64 {
+	ms.sc.Split(bufio.ScanWords)
+
+	ret := make([]int64, 0)
+
+	for i := 0; i < n; i++ {
+		ms.sc.Scan()
+		v, err := strconv.ParseInt(ms.sc.Text(), 10, 64)
+		if err != nil {
+			panic(err)
+		}
+		ret = append(ret, v)
+	}
+
+	return ret
+}
+
+func (ms *MyScanner) readInt() int {
+	var a int
+	fmt.Scan(&a)
+	return a
+}
+
+func (ms *MyScanner) readInt64() int64 {
+	var a int64
+	fmt.Scan(&a)
+	return a
 }
