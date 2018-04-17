e9b0509bc7ef2d419ec06c19f764f2f4f9faaaa2
ksakiyama <k.sakiyama1@gmail.com>
2018-04-17 11:38:42 +0900

    Add a struct

--- main.go
+++ main.go
@@ -8,6 +8,66 @@ import (
 	"strings"
 )
 
+// MyScanner : comment
+type MyScanner struct {
+	sc *bufio.Scanner
+}
+
+func newMyScanner() *MyScanner {
+	ms := new(MyScanner)
+	ms.sc = bufio.NewScanner(os.Stdin)
+	return ms
+}
+
+func (ms *MyScanner) readIntLine(n int) []int {
+	ms.sc.Split(bufio.ScanWords)
+
+	ret := make([]int, 0)
+
+	for i := 0; i < n; i++ {
+		ms.sc.Scan()
+		v, err := strconv.Atoi(ms.sc.Text())
+		if err != nil {
+			panic(err)
+		}
+		ret = append(ret, v)
+	}
+
+	return ret
+}
+
+func (ms *MyScanner) readInt64Line(n int) []int64 {
+	ms.sc.Split(bufio.ScanWords)
+
+	ret := make([]int64, 0)
+
+	for i := 0; i < n; i++ {
+		ms.sc.Scan()
+		v, err := strconv.ParseInt(ms.sc.Text(), 10, 64)
+		if err != nil {
+			panic(err)
+		}
+		ret = append(ret, v)
+	}
+
+	return ret
+}
+
+func (ms *MyScanner) readInt() int {
+	var a int
+	fmt.Scan(&a)
+	return a
+}
+
+func (ms *MyScanner) readInt64() int64 {
+	var a int64
+	fmt.Scan(&a)
+	return a
+}
+
+
+// ---------------------------------
+
 func getNumberOfDigits(n int64) int {
 	cnt := 0
 	for {
@@ -85,3 +145,5 @@ func sumInt64Array(arr []int64) int64 {
 	}
 	return sum
 }
+
+// --------------------------------------
